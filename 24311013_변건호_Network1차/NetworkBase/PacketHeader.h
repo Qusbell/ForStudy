3b6184d00c763293890df8565bf29192cf14bf93
Qusbell <01024072245a@gmail.com>
2026-04-12 14:43:28 +0900

    패킷 만들기

---
+++ 24311013_변건호_Network1차/NetworkBase/PacketHeader.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#define PACKET_MAGIC 0x50434B54 // 'PCKT'
+
+
+enum class PacketType : int
+{
+	PKT_CONNECT = 1,
+	PKT_CONNECT_ACK,
+	PKT_CHAT = 0x1001,
+	PKT_CHAT_BROADCAST = 0x2001,
+};
+
+
+#pragma pack(push, 1)
+struct PacketHeader
+{
+	int magic = PACKET_MAGIC;
+	int size;
+	PacketType type;
+};
+#pragma pack(pop)
