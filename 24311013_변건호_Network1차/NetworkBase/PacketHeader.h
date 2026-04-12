2f7c74ade6ef942061561465ca1f6e242367c2e5
Qusbell <01024072245a@gmail.com>
2026-04-12 14:56:54 +0900

    좀 보기좋게

--- 24311013_변건호_Network1차/NetworkBase/PacketHeader.h
+++ 24311013_변건호_Network1차/NetworkBase/PacketHeader.h
@@ -5,18 +5,31 @@
 
 enum class PacketType : int
 {
-	PKT_CONNECT = 1,
-	PKT_CONNECT_ACK,
-	PKT_CHAT = 0x1001,
-	PKT_CHAT_BROADCAST = 0x2001,
+	CONNECT = 1,
+	CONNECT_ACK,
+
+
+	//=== C to S (Request) ===//
+
+	CHAT = 0x1001,
+
+
+	//=== S to C (Broadcast) ===//
+
+	CHAT_BROADCAST = 0x2001
 };
 
 
 #pragma pack(push, 1)
 struct PacketHeader
 {
+	// 패킷 키
 	int magic = PACKET_MAGIC;
+	// 패킷 총 사이즈
 	int size;
+	// 패킷의 종류 (맥락)
 	PacketType type;
 };
 #pragma pack(pop)
+
+
