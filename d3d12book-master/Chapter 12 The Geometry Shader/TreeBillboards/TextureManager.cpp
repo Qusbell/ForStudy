f2c4b208f0b2172b3b7f9671cfb32d637d9a3a42
Standupdongmin <sdm031002@gmail.com>
2026-04-17 14:57:21 +0900

    texturemanager

--- d3d12book-master/Chapter 12 The Geometry Shader/TreeBillboards/TextureManager.cpp
+++ d3d12book-master/Chapter 12 The Geometry Shader/TreeBillboards/TextureManager.cpp
@@ -1,45 +1,41 @@
 #include "TextureManager.h"
 #include "../../Common/DDSTextureLoader.h"
 
-void TextureManager::LoadTextures(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList)
+TextureManager::TextureManager()
 {
-    auto grassTex = std::make_unique<Texture>();
-    grassTex->Name = "grassTex";
-    grassTex->Filename = L"../../Textures/grass.dds";
-    ThrowIfFailed(DirectX::CreateDDSTextureFromFile12(device,
-        cmdList, grassTex->Filename.c_str(),
-        grassTex->Resource, grassTex->UploadHeap));
-
-    auto waterTex = std::make_unique<Texture>();
-    waterTex->Name = "waterTex";
-    waterTex->Filename = L"../../Textures/water1.dds";
-    ThrowIfFailed(DirectX::CreateDDSTextureFromFile12(device,
-        cmdList, waterTex->Filename.c_str(),
-        waterTex->Resource, waterTex->UploadHeap));
-
-    auto fenceTex = std::make_unique<Texture>();
-    fenceTex->Name = "fenceTex";
-    fenceTex->Filename = L"../../Textures/WireFence.dds";
-    ThrowIfFailed(DirectX::CreateDDSTextureFromFile12(device,
-        cmdList, fenceTex->Filename.c_str(),
-        fenceTex->Resource, fenceTex->UploadHeap));
-
-    auto treeArrayTex = std::make_unique<Texture>();
-    treeArrayTex->Name = "treeArrayTex";
-    treeArrayTex->Filename = L"../../Textures/treeArray2.dds";
-    ThrowIfFailed(DirectX::CreateDDSTextureFromFile12(device,
-        cmdList, treeArrayTex->Filename.c_str(),
-        treeArrayTex->Resource, treeArrayTex->UploadHeap));
-
-    mTextures[grassTex->Name] = std::move(grassTex);
-    mTextures[waterTex->Name] = std::move(waterTex);
-    mTextures[fenceTex->Name] = std::move(fenceTex);
-    mTextures[treeArrayTex->Name] = std::move(treeArrayTex);
 }
 
+TextureManager::~TextureManager()
+{
+}
+
+void TextureManager::LoadTextures(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList)
+{
+    // [변경됨] 개별적으로 길게 하드코딩되던 텍스처 로드 로직을 LoadTexture 메서드를 통해 간결화했습니다.
+    LoadTexture("grassTex", L"../../Textures/grass.dds", device, cmdList);
+    LoadTexture("waterTex", L"../../Textures/water1.dds", device, cmdList);
+    LoadTexture("fenceTex", L"../../Textures/WireFence.dds", device, cmdList);
+    LoadTexture("treeArrayTex", L"../../Textures/treeArray2.dds", device, cmdList);
+}
+
+// [새로 추가됨] 매개변수를 기반으로 Texture 객체를 할당하고, DDS 파일을 로드하여 mTextures 맵에 보관합니다.
+void TextureManager::LoadTexture(const std::string& name, const std::wstring& filename, ID3D12Device* device, ID3D12GraphicsCommandList* cmdList)
+{
+    auto tex = std::make_unique<Texture>();
+    tex->Name = name;
+    tex->Filename = filename;
+    
+    ThrowIfFailed(DirectX::CreateDDSTextureFromFile12(device, cmdList, 
+                                                      tex->Filename.c_str(), 
+                                                      tex->Resource, 
+                                                      tex->UploadHeap));
+
+    mTextures[tex->Name] = std::move(tex);
+}
+
+// [복구됨] 원본 코드의 SRV 디스크립터 힙 빌드 부분 (변형 없이 그대로 합침)
 void TextureManager::BuildDescriptorHeaps(ID3D12Device* device)
 {
-    // 텍스처 개수만큼 SRV 힙을 생성합니다. 현재 4개.
     D3D12_DESCRIPTOR_HEAP_DESC srvHeapDesc = {};
     srvHeapDesc.NumDescriptors = 4;
     srvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
@@ -51,43 +47,59 @@ void TextureManager::BuildShaderResourceViews(ID3D12Device* device)
 {
     CD3DX12_CPU_DESCRIPTOR_HANDLE hDescriptor(mSrvDescriptorHeap->GetCPUDescriptorHandleForHeapStart());
 
-    // 맵에서 텍스처 리소스를 가져옵니다.
-    auto grassTex = mTextures.at("grassTex")->Resource;
-    auto waterTex = mTextures.at("waterTex")->Resource;
-    auto fenceTex = mTextures.at("fenceTex")->Resource;
-    auto treeArrayTex = mTextures.at("treeArrayTex")->Resource;
+    auto grassTex = mTextures["grassTex"]->Resource;
+    auto waterTex = mTextures["waterTex"]->Resource;
+    auto fenceTex = mTextures["fenceTex"]->Resource;
+    auto treeArrayTex = mTextures["treeArrayTex"]->Resource;
 
     D3D12_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
     srvDesc.Shader4ComponentMapping = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
     srvDesc.Format = grassTex->GetDesc().Format;
     srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;
     srvDesc.Texture2D.MostDetailedMip = 0;
-    srvDesc.Texture2D.MipLevels = -1;
+    srvDesc.Texture2D.MipLevels = grassTex->GetDesc().MipLevels;
+    srvDesc.Texture2D.ResourceMinLODClamp = 0.0f;
     device->CreateShaderResourceView(grassTex.Get(), &srvDesc, hDescriptor);
 
-    // 물 텍스처 SRV
+    // waterTex
     hDescriptor.Offset(1, device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV));
     srvDesc.Format = waterTex->GetDesc().Format;
+    srvDesc.Texture2D.MipLevels = waterTex->GetDesc().MipLevels;
     device->CreateShaderResourceView(waterTex.Get(), &srvDesc, hDescriptor);
 
-    // 철조망 텍스처 SRV
+    // fenceTex
     hDescriptor.Offset(1, device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV));
     srvDesc.Format = fenceTex->GetDesc().Format;
+    srvDesc.Texture2D.MipLevels = fenceTex->GetDesc().MipLevels;
     device->CreateShaderResourceView(fenceTex.Get(), &srvDesc, hDescriptor);
 
-    // 나무 텍스처 SRV (Texture2DArray 이므로 ViewDimension 변경 필요)
+    // treeArrayTex
     hDescriptor.Offset(1, device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV));
-    auto desc = treeArrayTex->GetDesc();
     srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2DARRAY;
     srvDesc.Format = treeArrayTex->GetDesc().Format;
     srvDesc.Texture2DArray.MostDetailedMip = 0;
-    srvDesc.Texture2DArray.MipLevels = -1;
+    srvDesc.Texture2DArray.MipLevels = treeArrayTex->GetDesc().MipLevels;
     srvDesc.Texture2DArray.FirstArraySlice = 0;
     srvDesc.Texture2DArray.ArraySize = treeArrayTex->GetDesc().DepthOrArraySize;
     device->CreateShaderResourceView(treeArrayTex.Get(), &srvDesc, hDescriptor);
 }
 
+Texture* TextureManager::GetTexture(const std::string& name)
+{
+    auto it = mTextures.find(name);
+    if (it != mTextures.end())
+    {
+        return it->second.get();
+    }
+    return nullptr;
+}
+
 const std::unordered_map<std::string, std::unique_ptr<Texture>>& TextureManager::GetTextures() const
 {
     return mTextures;
+}
+
+ID3D12DescriptorHeap* TextureManager::GetSrvDescriptorHeap() const
+{
+    return mSrvDescriptorHeap.Get();
 }
--- d3d12book-master/Chapter 12 The Geometry Shader/TreeBillboards/TextureManager.h
+++ d3d12book-master/Chapter 12 The Geometry Shader/TreeBillboards/TextureManager.h
@@ -5,31 +5,32 @@
 #include <string>
 #include <memory>
 
-// ==============================================================================
-// [Refactoring Phase 2-2] SRV(Shader Resource View) 생성 로직 통합
-// ==============================================================================
 class TextureManager
 {
 public:
-    TextureManager() = default;
-    ~TextureManager() = default;
+    TextureManager();
+    ~TextureManager();
 
-    // 1. 텍스처 데이터를 메모리에 로드합니다.
     void LoadTextures(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList);
 
-    // 2. 텍스처를 위한 Descriptor Heap을 생성합니다. (SRV 용)
+    // [새로 추가됨] 단일 텍스처를 로드하고 생성하는 헬퍼 메서드
+    void LoadTexture(const std::string& name, const std::wstring& filename, ID3D12Device* device, ID3D12GraphicsCommandList* cmdList);
+
+    // [복구됨] 원본에 있던 디스크립터 힙 생성 메서드
     void BuildDescriptorHeaps(ID3D12Device* device);
 
     // 3. 로드된 텍스처들의 SRV를 Heap에 생성(View 만들기)합니다.
     void BuildShaderResourceViews(ID3D12Device* device);
 
-    // Getter
+    Texture* GetTexture(const std::string& name);
     const std::unordered_map<std::string, std::unique_ptr<Texture>>& GetTextures() const;
-    ID3D12DescriptorHeap* GetSrvDescriptorHeap() const { return mSrvDescriptorHeap.Get(); }
+    
+    // [복구됨] SRV 힙 반환 메서드
+    ID3D12DescriptorHeap* GetSrvDescriptorHeap() const;
 
 private:
     std::unordered_map<std::string, std::unique_ptr<Texture>> mTextures;
-
-    // 텍스처 SRV들을 담아둘 서술자 힙
-    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> mSrvDescriptorHeap = nullptr;
+    
+    // [복구됨] SRV 디스크립터 힙
+    Microsoft::WRL::ComPtr<ID3D12DescriptorHeap> mSrvDescriptorHeap;
 };
