c8d2db0905b336a8b28be9a090327fa0e692e80d
Qusbell <01024072245a@gmail.com>
2026-04-18 13:44:38 +0900

    dd

---
+++ d3d12book-master/Chapter 12 The Geometry Shader/TreeBillboards/resource.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by MapTool.rc
+
+#define IDR_MAIN_MENU                   101
+#define ID_FILE_SAVE                    40001
+#define ID_FILE_LOAD                    40002
+#define ID_MODE_CAMERA                  40003
+#define ID_MODE_PLANT_TREE              40004
+#define ID_MODE_BUILD_FENCE             40005
+#define ID_ENV_DAY                      40006
+#define ID_ENV_NIGHT                    40007
