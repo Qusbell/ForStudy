31974ac54a7685f6dcc3472dc53b3a86f005c9d1
Qusbell <01024072245a@gmail.com>
2026-04-18 11:37:41 +0900

    ㅇㅇ

---
+++ d3d12book-master/Chapter 12 The Geometry Shader/TreeBillboards/TreeBillboardsApp.h
@@ -0,0 +1,81 @@
+﻿#pragma once
+
+#include "../../Common/d3dApp.h"
+#include "../../Common/MathHelper.h"
+#include "../../Common/UploadBuffer.h"
+#include "../../Common/GeometryGenerator.h"
+#include "FrameResource.h"
+#include "Waves.h"
+
+// 분리된 매니저 클래스 헤더
+#include "ResourceManager.h"
+#include "ShaderManager.h"
+#include "RenderItemManager.h"
+
+using Microsoft::WRL::ComPtr;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+#pragma comment(lib, "d3dcompiler.lib")
+#pragma comment(lib, "D3D12.lib")
+
+class TreeBillboardsApp : public D3DApp
+{
+public:
+    TreeBillboardsApp(HINSTANCE hInstance);
+    TreeBillboardsApp(const TreeBillboardsApp& rhs) = delete;
+    TreeBillboardsApp& operator=(const TreeBillboardsApp& rhs) = delete;
+    ~TreeBillboardsApp();
+
+    virtual bool Initialize()override;
+
+private:
+    virtual void OnResize()override;
+    virtual void Update(const GameTimer& gt)override;
+    virtual void Draw(const GameTimer& gt)override;
+
+    virtual void OnMouseDown(WPARAM btnState, int x, int y)override;
+    virtual void OnMouseUp(WPARAM btnState, int x, int y)override;
+    virtual void OnMouseMove(WPARAM btnState, int x, int y)override;
+
+    void OnKeyboardInput(const GameTimer& gt);
+    void UpdateCamera(const GameTimer& gt);
+    void AnimateMaterials(const GameTimer& gt);
+    void UpdateObjectCBs(const GameTimer& gt);
+    void UpdateMaterialCBs(const GameTimer& gt);
+    void UpdateMainPassCB(const GameTimer& gt);
+    void UpdateWaves(const GameTimer& gt);
+
+    void BuildDescriptorHeaps();
+    void BuildFrameResources();
+    void DrawRenderItems(ID3D12GraphicsCommandList* cmdList, const std::vector<RenderItem*>& ritems);
+
+private:
+
+    std::vector<std::unique_ptr<FrameResource>> mFrameResources;
+    FrameResource* mCurrFrameResource = nullptr;
+    int mCurrFrameResourceIndex = 0;
+
+    UINT mCbvSrvDescriptorSize = 0;
+
+    ComPtr<ID3D12DescriptorHeap> mSrvDescriptorHeap = nullptr;
+
+    // 분리된 매니저 클래스들
+    ResourceManager mResourceManager;
+    ShaderManager mShaderManager;
+    RenderItemManager mRenderItemManager;
+
+    std::unique_ptr<Waves> mWaves;
+
+    PassConstants mMainPassCB;
+
+    XMFLOAT3 mEyePos = { 0.0f, 0.0f, 0.0f };
+    XMFLOAT4X4 mView = MathHelper::Identity4x4();
+    XMFLOAT4X4 mProj = MathHelper::Identity4x4();
+
+    float mTheta = 1.5f * XM_PI;
+    float mPhi = XM_PIDIV2 - 0.1f;
+    float mRadius = 50.0f;
+
+    POINT mLastMousePos;
+};
